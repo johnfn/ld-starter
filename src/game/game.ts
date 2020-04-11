3f0913a918d90524d0cc15584ef5717a51f5ce6f
johnfn <johnfn@gmail.com>
2020-04-11 13:31:44 -0700

    Remove some unnecessary files.

--- src/game/game.ts
+++ src/game/game.ts
@@ -1,6 +1,5 @@
 import { BaseGame } from "../library/base_game";
 import { ResourcesToLoad } from "./resources";
-import { Entity } from "../library/entity";
 import { Player } from "./player";
 
 export class Game extends BaseGame<typeof ResourcesToLoad> {
