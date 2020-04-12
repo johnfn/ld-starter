02547be493637289081ab7fb7b8862544da29175
bharadr <46429739+bharadr@users.noreply.github.com>
2020-04-11 20:38:40 -0700

    Add bookshelf

--- src/game/game.ts
+++ src/game/game.ts
@@ -5,6 +5,7 @@ import { GameMap } from "./map";
 import { Test } from "./test";
 import { MyName } from "./whos_there";
 import { DialogBox } from "./dialog";
+import { Bookshelf } from "./bookshelf";
 
 export class Game extends BaseGame<typeof ResourcesToLoad> {
   public static Instance: Game;
@@ -25,6 +26,10 @@ export class Game extends BaseGame<typeof ResourcesToLoad> {
     this.stage.addChild(new Test());
 
     this.fixedCameraStage.addChild(new DialogBox());
+    let bookshelf = new Bookshelf(Game.Instance.assets.getResource("bookshelf.png").texture)
+    bookshelf.width = 20;
+    bookshelf.height = 50;
+    this.stage.addChild(bookshelf);
 
     if (MyName === "Grant") {
       // DialogBox.StartDialog(DialogTexts.IntroText);
