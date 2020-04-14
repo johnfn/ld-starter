48f75e89f502a0e60b3f15de0529daa57b3a734b
johnfn <johnfn@gmail.com>
2020-04-14 02:21:11 -0700

    O NO

--- src/game/tasukete_head.ts
+++ src/game/tasukete_head.ts
@@ -15,7 +15,8 @@ export class TasuketeHead extends Entity {
 
     this.sprite.anchor.set(0.5, 0.5);
 
-    this.startCoroutine("TasuketeAI", this.tasuketeAI());
+    this.startCoroutine("TasuketeMove", this.tasuketeMove());
+    this.startCoroutine("TasuketeAnimation", this.tasuketeAnimation());
 
     this.addOnMouseOver(() => { 
       this.scale = new Vector2(1.1, 1.1);
@@ -30,11 +31,47 @@ export class TasuketeHead extends Entity {
     })
   }
 
+  *tasuketeAnimation(): GameCoroutine {
+    const frames = [
+      Game.Instance.assets.getResource("tasukete/tasukete_0000.png").texture,
+      Game.Instance.assets.getResource("tasukete/tasukete_0001.png").texture,
+      Game.Instance.assets.getResource("tasukete/tasukete_0002.png").texture,
+      Game.Instance.assets.getResource("tasukete/tasukete_0003.png").texture,
+      Game.Instance.assets.getResource("tasukete/tasukete_0004.png").texture,
+      Game.Instance.assets.getResource("tasukete/tasukete_0005.png").texture,
+      Game.Instance.assets.getResource("tasukete/tasukete_0006.png").texture,
+      Game.Instance.assets.getResource("tasukete/tasukete_0007.png").texture,
+      Game.Instance.assets.getResource("tasukete/tasukete_0008.png").texture,
+      Game.Instance.assets.getResource("tasukete/tasukete_0009.png").texture,
+      Game.Instance.assets.getResource("tasukete/tasukete_0010.png").texture,
+      Game.Instance.assets.getResource("tasukete/tasukete_0011.png").texture,
+      Game.Instance.assets.getResource("tasukete/tasukete_0012.png").texture,
+      Game.Instance.assets.getResource("tasukete/tasukete_0013.png").texture,
+      Game.Instance.assets.getResource("tasukete/tasukete_0014.png").texture,
+      Game.Instance.assets.getResource("tasukete/tasukete_0015.png").texture,
+      Game.Instance.assets.getResource("tasukete/tasukete_0016.png").texture,
+      Game.Instance.assets.getResource("tasukete/tasukete_0017.png").texture,
+      Game.Instance.assets.getResource("tasukete/tasukete_0018.png").texture,
+    ];
+
+    let index = 0;
+
+    while (true) {
+      yield { frames: 5 };
+
+      this.texture = frames[++index % frames.length];
+    }
+  }
+
   dialogIndex = -1;
+  talking = false;
   startDialog() {
+    if (this.talking) { return; }
+
+    this.talking = true;
     this.dialogIndex++;
 
-    if (this.dialogIndex > DialogTexts.TasuketeInteractText.length) {
+    if (this.dialogIndex >= DialogTexts.TasuketeInteractText.length) {
       this.visible = false;
 
       return;
@@ -42,14 +79,15 @@ export class TasuketeHead extends Entity {
 
     const nextDialog = [DialogTexts.TasuketeInteractText[this.dialogIndex]];
 
-    this.startCoroutine("TasuketeDialog", DialogBox.StartDialog(nextDialog));
+    this.startCoroutine("TasuketeDialog", this.annoying(nextDialog));
   }
 
   *annoying(nextDialog: DialogText): GameCoroutine {
     yield* DialogBox.StartDialog(nextDialog);
+    this.talking = false;
   }
 
-  *tasuketeAI(): GameCoroutine {
+  *tasuketeMove(): GameCoroutine {
     const player = Player.Instance;
     const speed = 1.4;
 
